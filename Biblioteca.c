6a4db093e7910ee05fb11e0c62c78e8d01d95b39
MatheusSegatto <matheussegatto34@outlook.com>
2022-07-12 17:05:11 -0300

    Função remover e listar funcionando

--- Biblioteca.c
+++ Biblioteca.c
@@ -28,31 +28,6 @@ typedef struct
 } recurso;
 
 
-
-// void cadastro_aluno(alunos *cab){
-//     char matricula[20];
-//     char nome[50];
-//     //Perguntar matrícula, caso exista, dizer que já existe e voltar ao menu
-//     printf("Qual a matrícula do aluno?\n");
-//     scanf("%s", matricula);
-//     alunos *ant = NULL;
-//     alunos *p = pesquisa_aluno(1, matricula, cab, &ant);
-//     if (p != NULL)
-//     {
-//         printf("Aluno já cadastrado!\n");
-//         return;
-//     }
-//     p = (alunos *)malloc(sizeof(alunos));
-//     printf("Qual o nome do aluno?\n");
-//     scanf(" %[^\n]", nome);
-//     strcpy(p->nome, nome);
-//     strcpy(p->matricula, matricula);
-//     p->prox = cab->prox;
-//     cab->prox = p;
-//     printf("Aluno cadastrado com sucesso!\n");
-
-
-// } 
 void cadastro_aluno(alunos *p){
     char nome[50];
 
@@ -62,6 +37,26 @@ void cadastro_aluno(alunos *p){
     return;
 }
 
+void remove_aluno(alunos **vet){
+    int id = 0;
+    printf("Digite o ID do aluno que quer remover\n");
+    scanf("%d", &id);
+    //TODO Verificar se possui algo emprestado
+    //printf("%p\n", vet[id]);
+    free(vet[id]);
+    vet[id] = NULL;
+}
+
+void lista(alunos **vet, int quantidade){
+    for (int i = 0; i < quantidade; i++)
+    {
+        if (vet[i] != NULL)
+        {
+            printf("NOME: %s ID: %d\n", vet[i]->nome, i);
+        }
+    }
+    
+}
 
 //? alunos * pesquisa_aluno(int sw, char *procurado,alunos *cab, alunos **ant){}
 
@@ -103,8 +98,7 @@ void cadastro_recurso(){
 int main(int argc, char const *argv[])
 {
     //Alocando vetor de alunos
-    alunos **vetalunos = (alunos **)(malloc(sizeof(alunos *)));
-    vetalunos[0] = (alunos *)malloc(sizeof(alunos));
+    alunos **vetalunos = (alunos **)malloc(sizeof(alunos *));
     //vetalunos[i]->nome;
     //chamada -> rm_aluno(v[i])
     //rm_aluno(struct aluno *p)
@@ -112,17 +106,16 @@ int main(int argc, char const *argv[])
     ///p = NULL -> mesma coisa que p = malloc(0*sizeof(alunos));
 
     int matriculados = 0;
-    vetalunos = realloc(vetalunos, sizeof(35 * sizeof(alunos *)));
-    for (size_t i = 1; i < 35; i++)
+    vetalunos = realloc(vetalunos, 35 * sizeof(alunos *));
+    for (size_t i = 0; i < 35; i++)
     {
-        //a++;
-        //vetalunos = realloc(vetalunos, sizeof((a) * sizeof(alunos *)));
-        vetalunos[i] = (alunos *)(malloc(sizeof(alunos)));
+        vetalunos[i] = (alunos *)malloc(sizeof(alunos));
     }
+
     //Alocando vetor de livros
     livros **vetlivros = (livros **)(malloc(sizeof(livros *)));
     vetlivros[0] = (livros *)malloc(sizeof(livros));
-    vetlivros = realloc(vetlivros, sizeof(50 * sizeof(livros *)));
+    vetlivros = realloc(vetlivros, 50 * sizeof(livros *));
     for (size_t i = 1; i < 50; i++)
     {
         //a++;
@@ -134,41 +127,63 @@ int main(int argc, char const *argv[])
     //vetalunos[3]->teste = 56;
 
     int cadastrados = 0, turmas = 1;
-    int escolha;
-    while (escolha != 5)
+    int escolha = 1;
+    while (escolha != 0) //Menu Principal
     {
-        printf("MENU:\n1- Cadastrar aluno\n2- Cadastrar livro\n3- Cadastrar recurso\n4- Pesquisa\n5- Sair\n");
+        printf("MENU:\n1- Seção aluno\n2- Seção livro\n3- Seção recurso\n0- Sair\n");
         scanf("%d", &escolha);
         if (escolha == 1)
         {
-            matriculados +=1;
-            if (matriculados > 35)
-            {
-                //Aumentar vetor de alunos
-                turmas += 1;
-                vetalunos = realloc(vetalunos, sizeof((35 * turmas) * sizeof(alunos *)));
-            }
-            //Perguntar matrícula antes de entrar na função
-            printf("Qual a matrícula do aluno?\n");
-            char matricula[12];//Matrícula do cefet tem 11 caracteres ex: 2112227GCOM
-            scanf("%s", matricula);
-            int status = 0;
-            //Verficando se aluno já está matriculado
-            for (int i = 0; i < cadastrados; i++)
-            {
-                //TODO Como faria pra entrar na função sem modificar quantidade de cadastrados
-                if (strcmp(matricula, vetalunos[i]->matricula) == 0)
-                {
-                    printf("Aluno já cadastrado! ID: %d\n", i);
-                    status = 1;
-                    break;
+            while(escolha != 5){ //Menu Aluno
+                printf("MENU:\n1- Cadastrar aluno\n2- Remover aluno\n3- Listar alunos\n4- Pesquisar aluno\n5- Sair\n");
+                scanf("%d", &escolha);
+                if(escolha == 1){
+                    matriculados +=1;
+                    if (matriculados > (35 * turmas))
+                    {
+                        //Aumentar vetor de alunos
+                        turmas += 1;
+                        vetalunos = realloc(vetalunos, (35 * turmas) * sizeof(alunos *));
+                        for (size_t i = (35 * (turmas - 1)); i < (35 * turmas); i++)
+                        {
+                            vetalunos[i] = (alunos *)malloc(sizeof(alunos));
+                        }
+                        
+                    }
+                    //Perguntar matrícula antes de entrar na função
+                    printf("Qual a matrícula do aluno?\n");
+                    char matricula[12];//Matrícula do cefet tem 11 caracteres ex: 2112227GCOM
+                    scanf("%s", matricula);
+                    int status = 0;
+                    //Verficando se aluno já está matriculado
+                    for (int i = 0; i < cadastrados; i++)
+                    {
+                        //TODO Como faria pra entrar na função sem modificar quantidade de cadastrados
+                        if (strcmp(matricula, vetalunos[i]->matricula) == 0)
+                        {
+                            printf("Aluno já cadastrado! ID: %d\n", i);
+                            status = 1;
+                            break;
+                        }
+                    }
+                    if(status == 0)
+                    {
+                    strcpy(vetalunos[cadastrados]->matricula, matricula);
+                    cadastro_aluno(vetalunos[cadastrados]);
+                    cadastrados += 1;
+                    }
                 }
-            }
-            if(status == 0)
-            {
-            strcpy(vetalunos[cadastrados]->matricula, matricula);
-            cadastro_aluno(vetalunos[cadastrados]);
-            cadastrados += 1;
+                if (escolha == 2)
+                {
+                    remove_aluno(vetalunos);
+                }
+                if (escolha == 3)
+                {
+                    lista(vetalunos, cadastrados);
+                }
+                
+                
+
             }
         }
         else if (escolha == 2)
@@ -184,7 +199,7 @@ int main(int argc, char const *argv[])
             //pesquisa();
         }
     }
-    printf("%s\n", vetalunos[2]->nome);
+    //printf("%s\n", vetalunos[0]->nome);
     
     return 0;
 }
